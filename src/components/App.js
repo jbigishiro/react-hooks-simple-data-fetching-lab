4ddd016c174b4c4fb5d1a902f841babd69953717
Justin Bigishiro <justin.rvd@gmail.com>
2023-06-08 22:35:32 -0500

    adds myfile

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [image, setImage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <h3>Loading...</h3>;
+
+  return (
+    <div>
+      {" "}
+      <img src={image} alt=" A Random Dog" />{" "}
+    </div>
+  );
+}
